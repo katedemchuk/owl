0421150a039da00de085ec0e43b8d802bf45ea7b
Kateryna Demchuk <kate.v.demchuk@gmail.com>
2023-09-18 21:22:39 +0200

    Connect to MongoDB

---
+++ server/db/connection.mjs
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { MongoClient } from 'mongodb';
+
+const url = process.env.ATLAS_URI;
+const client = new MongoClient(url);
+const dbName = 'owl';
+
+let connection;
+try {
+  connection = await client.connect();
+  console.log('Connected successfully to the database server');
+} catch (error) {
+  console.error(error);
+}
+
+export const db = client.db(dbName);
